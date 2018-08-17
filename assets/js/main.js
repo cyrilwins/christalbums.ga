d1949d84856d870a98230b27398c16810d714a00
Cyril balraj <cyrilwins@gmail.com>
2018-08-17 22:18:29 +0600

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,118 +1,67 @@
-/*
-	Full Motion by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
 (function($) {
+    "use strict";
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+    jQuery(document).ready(function($) {
 
-	$(function() {
+        /*====  magnific popup js active =====*/
+        if ($.fn.magnificPopup) {
+            $('.play_video_, .play_you').magnificPopup({
+                type: 'video',
+                mainClass: 'mfp-iframe'
+            });
+        }
 
-		var $window = $(window),
-			$body = $('body');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        /*==== collapsed menu active =====*/
+        var menuCollapsed = $('.menu-icon, ul#nav > li > a');
+        menuCollapsed.on('click', function() {
+            $('body').toggleClass('expand');
+        })
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        if ($.fn.onePageNav) {
+            $('.one_page').onePageNav({
+                currentClass: 'active',
+                scrollSpeed: 750,
+                easing: 'swing'
+            });
+        }
 
-		// Banner.
-			var $banner = $('#banner');
 
-			if ($banner.length > 0) {
+        // -------------------------------------------------------------
+        // onepage nav start
+        // -------------------------------------------------------------
+        $('ul#nav > li > a, .hero-content > a').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+                location.hostname == this.hostname) {
 
-				// IE fix.
-					if (skel.vars.IEVersion < 12) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
 
-						$window.on('resize', function() {
 
-							var wh = $window.height() * 0.60,
-								bh = $banner.height();
 
-							$banner.css('height', 'auto');
 
-							window.setTimeout(function() {
+    });
 
-								if (bh < wh)
-									$banner.css('height', wh + 'px');
 
-							}, 0);
+    /*====  Window Load Function =====*/
+    jQuery(window).load(function() {
 
-						});
+        /*====  animation js =====*/
+        new WOW().init();
+        /*==== preloader active bottom =====*/
+        $('#loading').delay(1900).fadeOut('slow', function() { $(this).remove(); });
 
-						$window.on('load', function() {
-							$window.triggerHandler('resize');
-						});
+    });
 
-					}
 
-				// Video check.
-					var video = $banner.data('video');
 
-					if (video)
-						$window.on('load.banner', function() {
-
-							// Disable banner load event (so it doesn't fire again).
-								$window.off('load.banner');
-
-							// Append video if supported.
-								if (!skel.vars.mobile
-								&&	!skel.breakpoint('large').active
-								&&	skel.vars.IEVersion > 9)
-									$banner.append('<video autoplay loop><source src="' + video + '.mp4" type="video/mp4" /><source src="' + video + '.webm" type="video/webm" /></video>');
-
-						});
-
-				// More button.
-					$banner.find('.more')
-						.addClass('scrolly');
-
-			}
-
-		// Scrolly.
-			$('.scrolly').scrolly();
-
-		// Poptrox.
-			$window.on('load', function() {
-
-				var $thumbs = $('.thumbnails');
-
-				if ($thumbs.length > 0)
-					$thumbs.poptrox({
-						onPopupClose: function() { $body.removeClass('is-covered'); },
-						onPopupOpen: function() { $body.addClass('is-covered'); },
-						baseZIndex: 10001,
-						useBodyOverflow: false,
-						overlayColor: '#222226',
-						overlayOpacity: 0.75,
-						popupLoaderText: '',
-						fadeSpeed: 500,
-						usePopupDefaultStyling: false,
-						windowMargin: (skel.breakpoint('small').active ? 5 : 50)
-					});
-
-			});
-
-		// Initial scroll.
-			$window.on('load', function() {
-				$window.trigger('scroll');
-			});
-
-	});
-
-})(jQuery);
+}(jQuery));
---
+++ resources/js/main.js
@@ -0,0 +1,55 @@
+(function(){
+
+  var t = {
+    playlist: [
+      {
+        file: "resources/tracks/02.mp3",
+        thumb: "resources/thumbs/04.jpg",
+        trackName: "New song",
+        trackArtist: "Tobu & Syndec",
+        trackAlbum: "Single",
+      },
+
+
+
+
+      {
+        file: "resources/tracks/02.mp3",
+        thumb: "resources/thumbs/02.jpg",
+        trackName: "Blank",
+        trackArtist: "Disfigure",
+        trackAlbum: "Single",
+      },
+
+
+      {
+        file: "resources/tracks/04.mp3",
+        thumb: "resources/thumbs/04.jpg",
+        trackName: "Blank",
+        trackArtist: "Disfigure",
+        trackAlbum: "Single",
+      },
+
+
+      {
+        file: "resources/tracks/02.mp3",
+        thumb: "resources/thumbs/02.jpg",
+        trackName: "Blank",
+        trackArtist: "Disfigure",
+        trackAlbum: "Single",
+      },
+
+
+      {
+        file: "resources/tracks/03.mp3",
+        thumb: "resources/thumbs/03.jpg",
+        trackName: "Fade",
+        trackArtist: "Alan Walker",
+        trackAlbum: "Single",
+      }
+    ]
+  }
+
+  $(".jAudio").jAudio(t);
+
+})();
